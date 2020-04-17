4c45f7a546da9ed69ff478e75728d341089356e5
nkordis <nikoskordis@gmail.com>
2020-04-17 11:57:49 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text >
+       Quiz Cards
+      </Text>
+    </View>
+  );
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Quiz Cards
+
+# App Functionality
+
+## Create React Native App
