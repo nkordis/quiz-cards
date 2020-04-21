fb4ff0cb6b09ce52293b36c3f93eee0f7f1c3555
nkordis <nikoskordis@gmail.com>
2020-04-21 09:35:02 +0200

    feat: navigation to create new deck

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import * as React from "react";
-import { View } from "react-native";
+import { StatusBar } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
 import ListDecks from "./components/ListDecks";
+import NewDeck from "./components/NewDeck";
+
+const Tabs = createBottomTabNavigator({
+  Home: {
+    screen: ListDecks,
+  },
+  "New Deck": {
+    screen: NewDeck,
+  },
+});
 
 export default function App() {
-  return (
-    <View>
-      <ListDecks />
-    </View>
-  );
+  return <Tabs />;
 }
